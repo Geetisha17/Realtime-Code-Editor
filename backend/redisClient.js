d78c0fe61344bff9e8c9308ab9845ddb091c1eb7
Geetisha17 <geetishatandon@gmail.com>
2025-05-26 17:39:55 +0530

    changed redis url in backend

--- backend/redisClient.js
+++ backend/redisClient.js
@@ -1,7 +1,7 @@
 const redis = require('redis');
 
 const client = redis.createClient({
-  url: process.env.REDIS_URL || 'redis://redis:6379',
+  url: process.env.REDIS_URL ,
 });
 
 client.on('error', (err) => {
